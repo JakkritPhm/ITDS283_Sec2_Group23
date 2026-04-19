49101de9bdcde10047a9db86179e5ef8edefd20e
Thanarat Phonbunyawat <126804835+Filames@users.noreply.github.com>
2026-04-19 15:57:25 +0700

    Update LoginPage.dart

--- NasteFoodWo/lib/pages/LoginPage.dart
+++ NasteFoodWo/lib/pages/LoginPage.dart
@@ -160,13 +160,6 @@ class _LoginPageState extends State<LoginPage> {
                           ),
                         ],
                       ),
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'Forgot Password?',
-                          style: TextStyle(color: Colors.grey, fontSize: 14),
-                        ),
-                      ),
                     ],
                   ),
 
